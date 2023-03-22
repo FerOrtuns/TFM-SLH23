8974ec5379bdf60687db54d16bf9ee7266181188
L4u <l4u.keira@gmail.com>
2023-03-22 19:27:33 +0100

    Include sticky top

--- SLHApp/src/app/dashboard/components/head/head.component.ts
+++ SLHApp/src/app/dashboard/components/head/head.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Router} from "@angular/router";
+import {AuthService} from "../../../auth/services/auth.service";
 
 @Component({
   selector: 'app-head',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class HeadComponent {
 
+  constructor (private  router : Router,
+               private authService: AuthService ) { }
+
+  get user (){
+    return this.authService.user;
+  }
+
+  logout(){
+
+    this.router.navigateByUrl('./auth/login');
+
+    this.authService.logout();
+
+  }
 }
--- SLHApp/src/app/dashboard/components/menunav/menunav.component.ts
+++ SLHApp/src/app/dashboard/components/menunav/menunav.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-menunav',
@@ -8,19 +6,5 @@ import { AuthService } from 'src/app/auth/services/auth.service';
   styleUrls: ['./menunav.component.css']
 })
 export class MenunavComponent {
-  
-  constructor (private  router : Router,
-    private authService: AuthService ) { }
 
-get user (){
-return this.authService.user;
-}             
-
-logout(){
-
-this.router.navigateByUrl('./auth/login');
-
-this.authService.logout();
-
-}
 }
