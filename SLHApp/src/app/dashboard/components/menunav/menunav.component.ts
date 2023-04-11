bf5a070778e6bc794b40b545b140e7252c198ed4
FerOrtuns <fernando.ortunolopez@gmail.com>
2023-04-11 18:15:15 +0200

    commit plenty ok

--- SLHApp/src/app/dashboard/components/menunav/menunav.component.ts
+++ SLHApp/src/app/dashboard/components/menunav/menunav.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menunav',
   templateUrl: './menunav.component.html',
-  styleUrls: []
+  styles: [
+    `
+    .container{
+      min-height:72vh;
+    }
+    `
+  ]
 })
 export class MenunavComponent {
 
