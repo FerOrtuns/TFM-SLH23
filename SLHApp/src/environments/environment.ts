c521c35db327bfe41ccfc10f4538a276082a6df7
FerOrtuns <fernando.ortunolopez@gmail.com>
2023-04-16 03:33:48 +0200

    commit probando ya a ver si con el env de dev

--- SLHApp/src/environments/environment.ts
+++ SLHApp/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
 
     production: false,
-    baseUrl: 'http://localhost:4000/slh',
+    baseUrl: 'https://slhapp.onrender.com/slh',
     nbaApiKey:'83c3f2c9aa9a4595befc1c7b2116a3ff',
     nbaApiUrl: 'https://api.sportsdata.io/v3/nba/stats/json'
 };
